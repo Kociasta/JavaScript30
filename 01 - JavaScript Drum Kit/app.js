6fc538a1d28c5960d88114372a325360eb0917d0
Katarzyna Pieniazek <pieniazek.k@gmail.com>
2017-07-11 19:42:41 +0200

    that was easy :)

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded" , () => {
 
+    const keys = document.querySelectorAll('.key'); // save all keys in tab
+
     window.addEventListener('keydown' , function(e){
-        // e.keyCode
+        // e.keyCode --> get a code of the key
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
         if(!audio) {
             return
         }
-        audio.currentTime = 0;
-        audio.play();
+        audio.currentTime = 0; // this is for me to play immediately same button
+        audio.play(); //play sound
 
         key.classList.add('playing');
     })
 
-
+    keys.forEach( elem => { // run through the keys
+        elem.addEventListener('transitionend' , event => {
+            // lots of events - gimme only "transform" one
+            if(event.propertyName !== 'transform') return;
+            event.target.classList.remove('playing');
+        })
+    })
 
 });
